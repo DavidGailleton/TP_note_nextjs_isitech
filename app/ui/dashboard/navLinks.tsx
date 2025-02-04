1a8b5c9cb78cc942067d9b35f926c06fbc61c62a
David GAILLETON <david.gailleton@posteo.net>
2025-02-04 16:43:35 +0100

    ADD: Layout for dashboards (teacher only)

---
+++ app/ui/dashboard/navLinks.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Link from "next/link";
+import clsx from "clsx";
+import {usePathname} from "next/navigation";
+
+const links = [
+    {name: 'Courses', href: '/teacher/courses', role: 'teacher'},
+    {name: 'Students', href: '/teacher/students', role: 'teacher'},
+    {name: 'Evaluations', href: '/teacher/evaluations', role: 'teacher'},
+];
+
+export default function NavLinks() {
+    const pathname = usePathname();
+    return (
+        <>
+            {links.map((link) => {
+                if (1 == 1) { // TODO: Replace by: if user.role == link.role
+                    return (<Link
+                        key={link.name}
+                        href={link.href}
+                        className={clsx(
+                            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                            {
+                                'bg-sky-100 text-blue-600': pathname === link.href,
+                            },
+                        )}
+                        >
+                        <p className="hidden md:block">{link.name}</p>
+                    </Link>)
+                }
+            })}
+        </>
+    )
+}
