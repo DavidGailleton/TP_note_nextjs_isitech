c23bb4c00005972d0c80b7da0b1221124d682eb5
David GAILLETON <david.gailleton@posteo.net>
2025-02-05 09:45:34 +0100

    Course fetch with id

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,6 +1,6 @@
 "use server";
 import postgres from "postgres";
-import {coursesTable} from "@/app/lib/definitions";
+import {courseTable} from "@/app/lib/definitions";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -13,9 +13,9 @@ export async function fetchRoles() {
         throw new Error("Failed to fetch revenue data.");
     }
 }
-export async function fetchCourses():Promise<coursesTable[]>{
+export async function fetchCourses():Promise<courseTable[]>{
     try {
-        return await sql<coursesTable[]>`
+        return await sql<courseTable[]>`
             SELECT 
                 course.id,
                 course.title,
@@ -29,5 +29,28 @@ export async function fetchCourses():Promise<coursesTable[]>{
             JOIN users ON users.id = course.teacherid;`;
     } catch (error) {
         throw new Error("An error occurred on DB request")
+    }
+}
+
+export async function fetchCourse(id: string):Promise<courseTable>{
+    try {
+        const courses = await sql<courseTable[]>`
+            SELECT
+                course.id,
+                course.title,
+                course.description,
+                course.instrument,
+                course.teacherid,
+                users.name AS teacherName,
+                course.level,
+                course.schedule,
+                course.capacity
+            FROM course
+            JOIN users ON users.id = course.teacherid 
+            WHERE course.id = ${id};`;
+
+        return courses[0];
+    } catch (error) {
+        throw new Error("Course fetch failed")
     }
 }
