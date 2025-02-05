4c0e559e9460952224538f2d0491b6962c7b4c17
David GAILLETON <david.gailleton@posteo.net>
2025-02-05 09:09:56 +0100

    Courses type definition + fetch request for all
    # Conflicts:
    #	app/lib/data.ts
    #	app/lib/definitions.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,7 +1,8 @@
 "use server";
 import postgres from "postgres";
+import {coursesTable} from "@/app/lib/definitions";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRoles() {
     try {
@@ -10,5 +11,23 @@ export async function fetchRoles() {
     } catch (error) {
         console.error("Database Error:", error);
         throw new Error("Failed to fetch revenue data.");
+    }
+}
+export async function fetchCourses():Promise<coursesTable[]>{
+    try {
+        return await sql<coursesTable[]>`
+            SELECT 
+                course.id,
+                course.title,
+                course.description,
+                course.instrument,
+                users.name,
+                course.level,
+                course.schedule,
+                course.capacity
+            FROM course
+            JOIN users ON users.id = course.teacherid;`;
+    } catch (error) {
+        throw new Error("An error occurred on DB request")
     }
 }
--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -5,3 +5,14 @@ export type Role = {
 export type RegisterRole = {
     roles: Role[];
 };
+
+export type coursesTable = {
+    id: string,
+    title: string,
+    description: string,
+    instrument: string,
+    teacherName: string,
+    level: string,
+    schedule: number,
+    capacity: number,
+}
